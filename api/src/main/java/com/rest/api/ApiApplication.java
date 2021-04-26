20714fb715343c265d425e9f304db4806d858296
zziri <jihzang@naver.com>
2021-04-27 00:06:10 +0900

    security: add PasswordEncoder Bean
    passwordEncoder 빈을 사용하기 위해서 빈 추가

--- api/src/main/java/com/rest/api/ApiApplication.java
+++ api/src/main/java/com/rest/api/ApiApplication.java
@@ -2,6 +2,9 @@ package com.rest.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class ApiApplication {
@@ -10,4 +13,8 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 }
